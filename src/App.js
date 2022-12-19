d704355dc6821e6b19c4d75f671e3ecaedcddcef
khanhvtrng12 <105966854+khanhvtrng12@users.noreply.github.com>
2022-12-19 23:02:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  
+  
+  const [jobs, setJobs] = useState(()=>{
+    const StorageJobs = JSON.parse(localStorage.getItem('jobs'));
+    return StorageJobs ??[];
+  });
+  const [job, setJob] = useState('');
+  const handleSubmit = () => {
+    setJobs(prev => {
+      const newJobs = [...prev, job];
+      console.log(newJobs);
+      const jsonJobs = JSON.stringify(newJobs);
+      localStorage.setItem('jobs', jsonJobs);
+      return newJobs;
+    });
+    setJob('');
+  }
+  const DeleteJobs = (index) => {
+    const newJobs = jobs.filter((job, idx) => idx !== index);
+    console.log(newJobs);
+    localStorage.setItem('jobs', JSON.stringify(newJobs));
+    setJobs(newJobs);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1 className='Heading multicolortext'>ToDo List</h1>
+      
+        <div className='form-todo todo-list'>
+          <input 
+            className=' input-todo'
+            type="text"
+            laceholder="Nhập công việc" 
+            value={job}
+            onChange={e => setJob(e.target.value)}
+            
+          />
+          <button className='button-todo' onClick ={handleSubmit}>Thêm</button>
+        </div>
+        <ul className='todo-list pd-50 dp-flex-col'>
+          {jobs.map((job,index)=>(
+            <li className='dp-flex-row' key={index}>
+              <p>{job}</p>
+              <button className='button-todo' onClick={() => DeleteJobs(index)} style={{margin:20}}>Xóa</button>
+            </li>
+          ))}
+        </ul>
+       </div>
     </div>
   );
 }
